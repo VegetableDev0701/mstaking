f044fb343b8d0fb5733b8ae3d58149013ba09541
boranfurkan <furkanboran123@gmail.com>
2024-01-29 01:10:53 +0300

    feat: add hover and click effect to button.

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -9,15 +9,38 @@ interface ButtonProps {
 }
 
 const Button = ({ children, className, onClick, disabled }: ButtonProps) => {
+  const createRipple = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const button = event.currentTarget;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+    circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+  };
+
   return (
     <button
-      className={`flex-center px-3 py-2.5 rounded-lg text-[#F0F0F5] bg-primary max-md:px-[8.61px] max-md:py-[7.18px] ${
-        className && className
+      className={`relative overflow-hidden flex-center px-3 py-2.5 rounded-lg text-[#F0F0F5] bg-primary max-md:px-[8.61px] max-md:py-[7.18px] hover:opacity-85 transition-all ${
+        className ? className : ""
       }`}
-      onClick={onClick}
+      onClick={(e) => {
+        createRipple(e);
+        onClick();
+      }}
       disabled={disabled}
     >
-      <span className="text-base font-semibold leading-5 tracking-[0.01em] text-center max-md:text-sm">
+      <span className="text-base font-semibold leading-5 tracking-[0.01em] text-center max-md:text-sm z-10">
         {children}
       </span>
     </button>
