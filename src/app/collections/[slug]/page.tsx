4353bdbcb83c257929513fe789df28ea2963e331
boranfurkan <furkanboran123@gmail.com>
2024-02-10 18:16:09 +0000

    feat: airdrop panel page complete.

--- src/app/collections/[slug]/page.tsx
+++ src/app/collections/[slug]/page.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import { useParams } from "next/navigation";
 import CollectionBanner from "@/components/pages/collections/CollectionBanner";
 import CollectionTabs from "@/components/pages/collections/CollectionTabs";
-import { getCollectionData } from '@/lib/features/collectionSlice'
-import { setCollectionTokens, getCollectionTokens } from '@/lib/features/tokenSlice'
+import { getCollectionData } from "@/lib/features/collectionSlice";
+import {
+  setCollectionTokens,
+  getCollectionTokens,
+} from "@/lib/features/tokenSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { getCollectionNFT, getSMNFT } from "@/helper/queryHelper";
 import { Token } from "@/interface/token";
@@ -12,38 +15,61 @@ import { useEffect } from "react";
 import { Collection } from "@/interface/collection";
 import { CollectionToken } from "@/interface/token";
 const Page = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const { slug: Caddress } = useParams();
-  const selCollection: Collection = useSelector(getCollectionData(Caddress))
+  const selCollection: Collection = useSelector(getCollectionData(Caddress));
   const getNFTData = async () => {
-    const collectionNFT = await getCollectionNFT(Caddress)
-    const smNFT = await getSMNFT(selCollection.Saddress)
-    dispatch(setCollectionTokens({
-      keyname: `${Caddress}/${selCollection.Ctitle}`,
-      tokens: {
-        unstaked: collectionNFT,
-        staked: smNFT
-      }
-    }))
-  }
+    const collectionNFT = await getCollectionNFT(Caddress);
+    const smNFT = await getSMNFT(selCollection.Saddress);
+    dispatch(
+      setCollectionTokens({
+        keyname: `${Caddress}/${selCollection.Ctitle}`,
+        tokens: {
+          unstaked: collectionNFT,
+          staked: smNFT,
+        },
+      })
+    );
+  };
   useEffect(() => {
-    getNFTData()
-  }, [])
-  const selCollectionTokens: CollectionToken = useSelector(getCollectionTokens(`${Caddress}/${selCollection.Ctitle}`))
+    getNFTData();
+  }, []);
+  const selCollectionTokens: CollectionToken = useSelector(
+    getCollectionTokens(`${Caddress}/${selCollection.Ctitle}`)
+  );
 
   return selCollectionTokens ? (
     <div className="flex flex-col gap-5">
       <CollectionBanner
-        backgroundImage={selCollection?.CBackground ? selCollection?.CBackground : ''}
-        imageUrl={selCollection?.CBackground ? selCollection?.CBackground : ''}
-        longTitle={selCollection?.Ctitle ? selCollection?.Ctitle : ''}
-        description={selCollection?.Cdescription ? selCollection?.Cdescription : ''}
-        staked={
-          selCollectionTokens ? selCollectionTokens.staked.filter((el: Token) => el.start_timestamp > el.end_timestamp).length.toString() : "0"
+        backgroundImage={
+          selCollection?.CBackground ? selCollection?.CBackground : ""
         }
-        total={selCollectionTokens ? (selCollectionTokens.staked.filter((el: Token) =>  el.start_timestamp > el.end_timestamp).length + selCollectionTokens.unstaked.length).toString() : "0"}
+        imageUrl={selCollection?.CBackground ? selCollection?.CBackground : ""}
+        longTitle={selCollection?.Ctitle ? selCollection?.Ctitle : ""}
+        description={
+          selCollection?.Cdescription ? selCollection?.Cdescription : ""
+        }
+        staked={
+          selCollectionTokens
+            ? selCollectionTokens.staked
+                .filter((el: Token) => el.start_timestamp > el.end_timestamp)
+                .length.toString()
+            : "0"
+        }
+        total={
+          selCollectionTokens
+            ? (
+                selCollectionTokens.staked.filter(
+                  (el: Token) => el.start_timestamp > el.end_timestamp
+                ).length + selCollectionTokens.unstaked.length
+              ).toString()
+            : "0"
+        }
       />
-      <CollectionTabs tokens={selCollectionTokens} selCollection={selCollection}/>
+      <CollectionTabs
+        tokens={selCollectionTokens}
+        selCollection={selCollection}
+      />
     </div>
   ) : (
     <div>
--- src/app/owner-panel/page.tsx
+++ src/app/owner-panel/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+import AirdropPanel from "@/components/pages/owner-panel/AirdropPanel";
 import OwnerCollectionCard from "@/components/pages/owner-panel/OwnerCollectionCard";
 import React from "react";
 
 const Page = () => {
+  const [activeTab, setActiveTab] = React.useState("collections");
   return (
-    <div>
-      <OwnerCollectionCard />
+    <div className="flex flex-col gap-6">
+      <div className="flex items-center justify-center gap-5 text-xl font-medium">
+        <div
+          className={`w-full flex items-center justify-center p-3 bg-dark-700 rounded-lg border border-dark-600 uppercase text-dark-200 cursor-pointer transition-all ${
+            activeTab === "collections" && "!bg-dark-500 text-white"
+          }`}
+          onClick={() => setActiveTab("collections")}
+        >
+          Collections
+        </div>
+        <div
+          className={`w-full flex items-center justify-center p-3 bg-dark-700 rounded-lg border border-dark-600 uppercase text-dark-200 cursor-pointer transition-all ${
+            activeTab === "airdrop" && "!bg-dark-500 text-white"
+          }`}
+          onClick={() => setActiveTab("airdrop")}
+        >
+          Airdrop Panel
+        </div>
+      </div>
+      <div>
+        {activeTab === "collections" && <OwnerCollectionCard />}
+        {activeTab === "airdrop" && <AirdropPanel />}
+      </div>
     </div>
   );
 };
